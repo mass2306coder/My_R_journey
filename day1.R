ea6a216dd2bebe98ae2eeed6ea5a7641f7a4a26b
Masha <mashasrinivasan23@gmail.com>
2026-04-30 13:33:28 +0530

    Add files via upload
    Day-1
    Prints a message to the console
    Creates two variables (a and b)
    Adds them together
    Displays the result

---
+++ day1.R
@@ -0,0 +1,14 @@
+# Simple R program
+
+# Print a message
+print("Hello, World!")
+
+# Create variables
+a <- 10
+b <- 5
+
+# Perform addition
+sum <- a + b
+
+# Display the result
+print(paste("The sum is:", sum))
